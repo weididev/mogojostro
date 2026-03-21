adcc238efb32a8d5997e6dda177a241151a8c3e6
weididev <snehamoydutta11@gmail.com>
2026-03-21 15:53:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // আপনার গিটহাব রিপোজিটরির নাম যদি "mogojostro" হয়, তাহলে নিচের লাইনটি দিন:
-  base: '/আপনার-রিপোজিটরির-নাম/', 
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
-});
+  plugins: [react()],
+  base: './', // এটি অবশ্যই থাকতে হবে যাতে সাদা স্ক্রিন না আসে
+})
