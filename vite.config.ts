63c68dfdbc9c79071f310ccd7b5496995e219f9b
weididev <snehamoydutta11@gmail.com>
2026-03-21 12:54:02 +0530

    Set base path for GitHub repository
    Added base path configuration for GitHub repository.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  // আপনার গিটহাব রিপোজিটরির নাম যদি "mogojostro" হয়, তাহলে নিচের লাইনটি দিন:
+  base: '/আপনার-রিপোজিটরির-নাম/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
